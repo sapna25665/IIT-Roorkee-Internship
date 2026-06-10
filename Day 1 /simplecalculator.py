760b83d19b64d4b952426d2dc6645d674f0c0ed5
sapna25665 <sapnaduhan450@gmail.com>
2026-06-10 14:10:37 +0530

    Implement addition, subtraction, multiplication, and division
    Added basic arithmetic operations to the calculator.

--- Day 1 /simplecalculator.py
+++ Day 1 /simplecalculator.py
@@ -1,1 +1,9 @@
-
+num1 = float(input("enter first number"))
+num2 = float(input("enter second number"))
+print('Addition=',num1+num2)
+print("Subtraction=",num1-num2)
+print("Multiplication=",num1*num2)
+if num2!=0:
+     print("Division=",num1/num2)
+else:
+      print("Division by zero is not allowed.")

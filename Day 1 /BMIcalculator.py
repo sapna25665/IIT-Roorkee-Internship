289f09deb62650bbd29f134e0584dbe4b8c7e425
sapna25665 <sapnaduhan450@gmail.com>
2026-06-10 14:12:30 +0530

    Implement BMI calculator with category classification

--- Day 1 /BMIcalculator.py
+++ Day 1 /BMIcalculator.py
@@ -1,1 +1,12 @@
-
+height = float(input('enter height in meters'))
+weight = float(input('enter height in kg'))
+BMI = weight/(height**2)
+print('BMI = ',round(BMI,2))
+if BMI < 18.5:
+    print('category : underweight')
+elif  BMI<25:
+    print('category: normal weight')
+elif BMI<30:
+    print('category : overweight')
+else:
+    print('category : obese')        

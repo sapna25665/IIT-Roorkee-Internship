29f0a174d3d7aaf10926a2a1179c54a238d69439
sapna25665 <sapnaduhan450@gmail.com>
2026-06-10 14:06:02 +0530

    Update Greeting.py

--- Day 1 /Greeting.py
+++ Day 1 /Greeting.py
@@ -1,1 +1,11 @@
-
+name = input("enter your name")
+age = input("enter your age")
+print(f"hello {name}, are {age} old")
+a = 10
+b = 3.14
+c = "Python"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))

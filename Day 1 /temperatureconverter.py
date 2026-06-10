f74be9bd499b0d5a5b24ba5975c809ac90cd7119
sapna25665 <sapnaduhan450@gmail.com>
2026-06-10 14:14:32 +0530

    Add temperature conversion between Celsius and Fahrenheit

--- Day 1 /temperatureconverter.py
+++ Day 1 /temperatureconverter.py
@@ -1,1 +1,6 @@
-
+celsius =float(input("enter temperature in celsius"))
+fahrenheit = (celsius *9/5)+32
+print("temperature in fahrenheit",fahrenheit)
+fahrenheit=float(input("enter temperature in fahrenheit"))
+celsius = (fahrenheit-32)*5/9
+print("temperature in celsius",celsius)

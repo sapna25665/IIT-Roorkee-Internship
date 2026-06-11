737ecb28ffe09ebd31cacfd06d24379ecb19e7e1
sapna25665 <sapnaduhan450@gmail.com>
2026-06-11 12:48:23 +0530

    Update numberguess.py

--- Day2/numberguess.py
+++ Day2/numberguess.py
@@ -1,1 +1,11 @@
-
+secret_number = 7
+guess = 0
+while guess !=secret_number:
+    guess = int(input("guess the number (1-10)"))
+    if guess < secret_number:
+        print("too low")
+    elif guess > secret_number:
+        print("too high")
+    else:
+        print("congratulation! you guessed it.")
+            

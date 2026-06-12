4c32c31cf50a47cbfc0cc3b16161fa9fe9c588ad
sapna25665 <sapnaduhan450@gmail.com>
2026-06-12 16:29:30 +0530

    Update Factorial_calculator.py

--- Day 3/Factorial_calculator.py
+++ Day 3/Factorial_calculator.py
@@ -1,1 +1,14 @@
-
+#Iterative Approach
+num = int(input("Enter a number :"))
+fact = 1
+for i in range(1,num+1):
+    fact*=i
+print("Factorial =",fact)               
+#Recursive Approach
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+    num = int(input("enter a number:"))
+    print("Factorial =",factorial))

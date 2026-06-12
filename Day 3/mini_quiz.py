128d522c44411eb894e0f6d8e8f1ca648a32614e
sapna25665 <sapnaduhan450@gmail.com>
2026-06-12 15:56:12 +0530

    Add mini quiz application in mini_quiz.py

---
+++ Day 3/mini_quiz.py
@@ -0,0 +1,13 @@
+score = 0
+print("Mini quizz app")
+if input("1. Capital of india ?")=="Delhi":
+    score+=1
+if input("2. 3+5 =?")=="8":
+    score+=1
+if input ("3. Python is a programing language?(yes/no)")=="yes":
+    score+=1
+if input("4. Largest ocean in the world?")=="Pacific":
+    score+=1
+if input("5. 15*2 =?")=="30":
+     score+=1
+     print(f"Your score:", score, "/5")           

636b304f41873b01363338b239024228406ab4b6
sapna25665 <sapnaduhan450@gmail.com>
2026-06-12 14:38:35 +0530

    Update Palindrome.py

--- Day 3/Palindrome.py
+++ Day 3/Palindrome.py
@@ -1,1 +1,8 @@
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+n = int(input("enter a number :"))
 
+if is_palindrome(n):
+    print("Palindromr Number")
+else:
+    print("Not a Palindrome Number")
